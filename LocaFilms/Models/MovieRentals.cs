f6780048c807c3fa7e6736a25dd8b5c9cd9a1d28
levisonjr <levisonjr21@gmail.com>
2024-03-12 23:01:47 -0300

    Completando as propriedades da model

--- LocaFilms/Models/MovieRentals.cs
+++ LocaFilms/Models/MovieRentals.cs
@@ -1,11 +1,17 @@
-﻿namespace LocaFilms.Models
+﻿using LocaFilms.Enums;
+
+namespace LocaFilms.Models
 {
     public class MovieRentals
     {
         public int UserId { get; set; }
-        public UserModel User { get; set; }
-
         public int MovieId { get; set; }
+        public DateTime RentalStartDate { get; set; }
+        public DateTime RentalEndDate { get; set; }
+        public RentalStatusEnum RentalStatus { get; set; }
+        public PaymentStatusEnum PaymentStatus { get; set; }
+
+        public UserModel User { get; set; }
         public MovieModel Movie { get; set; }
     }
 }
