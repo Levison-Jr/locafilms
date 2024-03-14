2ada89bd0d24ed723dbab8b813a6f98353b3a5f6
levisonjr <levisonjr21@gmail.com>
2024-03-13 22:32:38 -0300

    Adicionando endpoints GET (GetAllUsers) e POST (CreateUser - incompleto)

--- LocaFilms/Controllers/UsersController.cs
+++ LocaFilms/Controllers/UsersController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LocaFilms.Dtos;
+using LocaFilms.Models;
+using LocaFilms.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LocaFilms.Controllers
@@ -7,10 +11,33 @@ namespace LocaFilms.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
-        [HttpGet]
-        public string GetAllUsers()
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
         {
-            return "Hello, World!";
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var result = await _userService.GetAllUsersAsync();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(UserModel user)
+        {
+            var result = await _userService.CreateUserAsync(user);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+
+            return CreatedAtAction(
+                actionName: nameof(CreateUser),
+                routeValues: new { id = user.Id },
+                value: result.User);
         }
     }
 }
