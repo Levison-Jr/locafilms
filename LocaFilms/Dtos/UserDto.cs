7a361daf3faf6bc4f85d3eefb1b4abe65cfd4afc
levisonjr <levisonjr21@gmail.com>
2024-03-15 21:15:55 -0300

    Adicionado DTO para User

---
+++ LocaFilms/Dtos/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using LocaFilms.Enums;
+using LocaFilms.Models;
+
+namespace LocaFilms.Dtos
+{
+    public record UserDto(
+        int Id,
+        string Name,
+        string Username,
+        string Email,
+        string Phone,
+        PerfilEnum Perfil,
+        bool IsActive,
+        DateTime RegistrationDate,
+        decimal Balance);
+}
