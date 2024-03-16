e1b8944e777dbaaba805d84f4da599a89b7a1d7b
levisonjr <levisonjr21@gmail.com>
2024-03-15 21:16:49 -0300

    Config de Perfil para conversão do AutoMapper

---
+++ LocaFilms/Mapping/DtoToModelProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using LocaFilms.Dtos;
+using LocaFilms.Models;
+
+namespace LocaFilms.Mapping
+{
+    public class DtoToModelProfile : Profile
+    {
+        public DtoToModelProfile()
+        {
+            CreateMap<CreateUserDto, UserModel>()
+                .ForMember(dest =>
+                dest.IsActive,
+                opt => opt.MapFrom(_ => true))
+
+                .ForMember(dest =>
+                dest.RegistrationDate,
+                opt => opt.MapFrom(_ => DateTime.Now))
+
+                .ForMember(dest =>
+                dest.Balance,
+                opt => opt.MapFrom(_ => 0));
+        }
+    }
+}
---
+++ LocaFilms/Mapping/ModelToDtoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LocaFilms.Dtos;
+using LocaFilms.Models;
+
+namespace LocaFilms.Mapping
+{
+    public class ModelToDtoProfile : Profile
+    {
+        public ModelToDtoProfile()
+        {
+            CreateMap<UserModel, UserDto>();
+        }
+    }
+}
