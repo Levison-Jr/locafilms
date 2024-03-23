e4d4b065878ff877797183e30d80255efe99f40d
levisonjr <levisonjr21@gmail.com>
2024-03-22 21:26:02 -0300

    Adicionado validações para todos os campos do CreateMovieDto (DataAnnotations)

--- LocaFilms/Dtos/CreateMovieDto.cs
+++ LocaFilms/Dtos/CreateMovieDto.cs
@@ -1,11 +1,23 @@
 ﻿using LocaFilms.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace LocaFilms.Dtos
 {
-    public record CreateMovieDto(
-        string Title,
-        string Description,
-        CategoryEnum Category,
-        decimal CostPerDay,
-        int NumberPhysicalMedia);
+    public record CreateMovieDto
+    {
+        [Required(ErrorMessage = "O título é obrigatório.")]
+        public string Title { get; init; }
+
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        public string Description { get; init; }
+
+        [EnumDataType(typeof(CategoryEnum), ErrorMessage = "O valor para Category é inválido.")]
+        public CategoryEnum Category { get; init; }
+
+        [Range(0.01, 100, ErrorMessage = "O valor para CostPerDay deve estar entre 0.01 e 100")]
+        public decimal CostPerDay { get; init; }
+
+        [Range(1, 100, ErrorMessage = "O valor para NumberPhysicalMedia deve estar entre 1 e 100")]
+        public int NumberPhysicalMedia { get; init; }
+    }
 }
