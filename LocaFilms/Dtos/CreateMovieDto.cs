241fb16f4d1a71531c194b781cfc2b761f6c3eda
levisonjr <levisonjr21@gmail.com>
2024-03-26 00:16:47 -0300

    Adicionada validação completa para DTO's, de request (create e update), do Movie

--- LocaFilms/Dtos/CreateMovieDto.cs
+++ LocaFilms/Dtos/CreateMovieDto.cs
@@ -5,19 +5,21 @@ namespace LocaFilms.Dtos
 {
     public record CreateMovieDto
     {
-        [Required(ErrorMessage = "O título é obrigatório.")]
-        public string Title { get; init; }
+        [Required(ErrorMessage = "O campo 'Title' é obrigatório.")]
+        public string? Title { get; init; }
 
-        [Required(ErrorMessage = "A descrição é obrigatória.")]
-        public string Description { get; init; }
+        [Required(ErrorMessage = "O campo 'Description' é obrigatória.")]
+        public string? Description { get; init; }
 
         [EnumDataType(typeof(CategoryEnum), ErrorMessage = "O valor para Category é inválido.")]
         public CategoryEnum Category { get; init; }
 
-        [Range(0.01, 100, ErrorMessage = "O valor para CostPerDay deve estar entre 0.01 e 100")]
+        [Required(ErrorMessage = "O campo 'CostPerDay' é obrigatório.")]
+        [Range(0.01, 100, ErrorMessage = "O valor para 'CostPerDay' deve estar entre 0.01 e 100")]
         public decimal CostPerDay { get; init; }
 
-        [Range(1, 100, ErrorMessage = "O valor para NumberPhysicalMedia deve estar entre 1 e 100")]
+        [Required(ErrorMessage = "O campo 'NumberPhysicalMedia' é obrigatório.")]
+        [Range(1, 100, ErrorMessage = "O valor para 'NumberPhysicalMedia' deve estar entre 1 e 100")]
         public int NumberPhysicalMedia { get; init; }
     }
 }
