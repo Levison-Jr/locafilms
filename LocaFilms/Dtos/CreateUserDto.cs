b4e640b953fceb3dd8251b6cc6ead1ba8d97619f
levisonjr <levisonjr21@gmail.com>
2024-03-26 21:15:48 -0300

    Adicionado as validações para DTOs de request do user (create e update)

--- LocaFilms/Dtos/CreateUserDto.cs
+++ LocaFilms/Dtos/CreateUserDto.cs
@@ -1,12 +1,26 @@
 ﻿using LocaFilms.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace LocaFilms.Dtos
 {
-    public record CreateUserDto(
-        string Name,
-        string Username,
-        string Password,
-        string Email,
-        string Phone,
-        PerfilEnum Perfil);
+    public record CreateUserDto
+    {
+        [Required(ErrorMessage = "O campo 'Name' é obrigatório.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "O campo 'Username' é obrigatório.")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "O campo 'Password' é obrigatório.")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "O campo 'Email' é obrigatório.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "O campo 'Phone' é obrigatório.")]
+        public string? Phone { get; set; }
+
+        [EnumDataType(typeof(PerfilEnum), ErrorMessage = "O valor para Perfil é inválido.")]
+        public PerfilEnum Perfil { get; set; }
+    }
 }
--- LocaFilms/Dtos/UpdateUserDto.cs
+++ LocaFilms/Dtos/UpdateUserDto.cs
@@ -1,13 +1,29 @@
 ﻿using LocaFilms.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace LocaFilms.Dtos
 {
-    public record UpdateUserDto(
-        string Name,
-        string Username,
-        string Password,
-        string Email,
-        string Phone,
-        PerfilEnum Perfil,
-        bool IsActive);
+    public record UpdateUserDto
+    {
+        [Required(ErrorMessage = "O campo 'Name' é obrigatório.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "O campo 'Username' é obrigatório.")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "O campo 'Password' é obrigatório.")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "O campo 'Email' é obrigatório.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "O campo 'Phone' é obrigatório.")]
+        public string? Phone { get; set; }
+
+        [EnumDataType(typeof(PerfilEnum), ErrorMessage = "O valor para Perfil é inválido.")]
+        public PerfilEnum Perfil { get; set; }
+
+        [Required(ErrorMessage = "O campo 'IsActive' é obrigatório.")]
+        public bool? IsActive { get; set; }
+    }
 }
