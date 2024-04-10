af689590048feba913b30b310e8e909088a61ec2
levisonjr <levisonjr21@gmail.com>
2024-04-09 23:24:27 -0300

    Tornando userId e movieId obrigatórios na request

--- LocaFilms/Dtos/CreateRentalDto.cs
+++ LocaFilms/Dtos/CreateRentalDto.cs
@@ -6,10 +6,10 @@ namespace LocaFilms.Dtos
     public record CreateRentalDto
     {
         [Required(ErrorMessage = "O campo UserId é obrigatório.")]
-        public int UserId { get; set; }
+        public int? UserId { get; set; }
 
         [Required(ErrorMessage = "O campo MovieId é obrigatório.")]
-        public int MovieId { get; set; }
+        public int? MovieId { get; set; }
 
         [Required(ErrorMessage = "O campo RentalStartDate é obrigatório.")]
         public DateTime? RentalStartDate { get; set; }
