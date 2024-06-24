65d55c1dd5dfa44dcf6872a8bb33f28f2558c229
levisonjr <levisonjr21@gmail.com>
2024-06-23 22:23:07 -0300

    Add Service para o Identity - feito o metodo para registro de user

---
+++ LocaFilms/Services/Identity/IIdentityService.cs
@@ -0,0 +1,11 @@
+﻿using LocaFilms.Dtos.Request;
+using LocaFilms.Dtos.Response;
+using LocaFilms.Services.Communication;
+
+namespace LocaFilms.Services.Identity
+{
+    public interface IIdentityService
+    {
+        Task<UserResponse> Register(CreateUserDto createUserDto); 
+    }
+}
---
+++ LocaFilms/Services/Identity/IdentityService.cs
@@ -0,0 +1,43 @@
+﻿using LocaFilms.Dtos.Request;
+using LocaFilms.Dtos.Response;
+using LocaFilms.Models;
+using LocaFilms.Services.Communication;
+using Microsoft.AspNetCore.Identity;
+
+namespace LocaFilms.Services.Identity
+{
+    public class IdentityService : IIdentityService
+    {
+        private readonly AspNetUserManager<UserModel> _aspNetUserManager;
+        public IdentityService(AspNetUserManager<UserModel> aspNetUserManager)
+        {
+            _aspNetUserManager = aspNetUserManager;
+        }
+
+        public async Task<UserResponse> Register(CreateUserDto createUserDto)
+        {
+            var user = new UserModel()
+            {
+                Email = createUserDto.Email,
+                UserName = createUserDto.Email,
+                EmailConfirmed = true
+            };
+
+            try
+            {
+                var result = await _aspNetUserManager.CreateAsync(user, createUserDto.Password ?? "");
+
+                if (!result.Succeeded)
+                {
+                    return new UserResponse($"Não foi possível cadastrar o usuário. ${result.Errors.Select(e => e.Description).First()}");
+                }
+
+                return new UserResponse(user);
+            }
+            catch (Exception ex)
+            {
+                return new UserResponse($"Não foi possível cadastrar o usuário. ${ex.Message}");
+            }
+        }
+    }
+}
