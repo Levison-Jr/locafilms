2484310c26c597790c50bade08a3468ec71785b5
levisonjr <levisonjr21@gmail.com>
2024-06-23 22:22:07 -0300

    Add RegEx para maiuscula, minuscula e numeros

--- LocaFilms/Dtos/Request/CreateUserDto.cs
+++ LocaFilms/Dtos/Request/CreateUserDto.cs
@@ -5,22 +5,16 @@ namespace LocaFilms.Dtos.Request
 {
     public record CreateUserDto
     {
-        [Required(ErrorMessage = "O campo 'Name' é obrigatório.")]
-        public string? Name { get; set; }
-
-        [Required(ErrorMessage = "O campo 'Username' é obrigatório.")]
-        public string? Username { get; set; }
-
-        [Required(ErrorMessage = "O campo 'Password' é obrigatório.")]
-        public string? Password { get; set; }
-
-        [Required(ErrorMessage = "O campo 'Email' é obrigatório.")]
+        [Required(ErrorMessage = $"O campo {nameof(Email)} é obrigatório.")]
+        [EmailAddress(ErrorMessage = $"O campo {nameof(Email)} é inválido.")]
         public string? Email { get; set; }
 
-        [Required(ErrorMessage = "O campo 'Phone' é obrigatório.")]
-        public string? Phone { get; set; }
+        [Required(ErrorMessage = "O campo 'Password' é obrigatório.")]
+        [StringLength(30, MinimumLength = 6, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9-._@+]+$", ErrorMessage = $"O campo {nameof(Password)} deve conter letra maiúscula, minúscula e número.")]
+        public string? Password { get; set; }
 
-        [EnumDataType(typeof(PerfilEnum), ErrorMessage = "O valor para Perfil é inválido.")]
-        public PerfilEnum Perfil { get; set; }
+        [Compare(nameof(Password), ErrorMessage = $"Os campos {nameof(Password)} e {nameof(PasswordConfirm)} devem ser iguais.")]
+        public string? PasswordConfirm { get; set; }
     }
 }
