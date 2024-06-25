53ffe2ba859ce1e2a13400bf5faea7a22743e1c4
levisonjr <levisonjr21@gmail.com>
2024-06-25 16:28:49 -0300

    Add injeção de dependencia para IdentityService e AspNetUserManager

--- LocaFilms/Program.cs
+++ LocaFilms/Program.cs
@@ -2,12 +2,10 @@ using LocaFilms.Contexts;
 using LocaFilms.Models;
 using LocaFilms.Repository;
 using LocaFilms.Services;
+using LocaFilms.Services.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Authorization;
 
 namespace LocaFilms
 {
@@ -39,15 +37,32 @@ namespace LocaFilms
             builder.Services.AddScoped<IRentalRepository, RentalRepository>();
             builder.Services.AddScoped<IRentalService, RentalService>();
 
+            builder.Services.AddScoped<IIdentityService, IdentityService>();
+            builder.Services.AddScoped<AspNetUserManager<UserModel>>();
+
             builder.Services.AddAutoMapper(typeof(Program));
 
             builder.Services.AddAuthorization();
             builder.Services.AddIdentityApiEndpoints<UserModel>()
                 .AddEntityFrameworkStores<AppDbContext>();
 
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // User settings.
+                options.User.RequireUniqueEmail = true;
+            });
+
             var app = builder.Build();
             
-            app.MapIdentityApi<UserModel>();
+            // app.MapIdentityApi<UserModel>();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
