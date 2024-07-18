dfa0235bbbcb042874c9da496efb7ef6e85cb14b
levisonjr <levisonjr21@gmail.com>
2024-07-18 20:11:54 -0300

    Remoção de propriedades não utilizadas no updade de usuários

--- LocaFilms/Dtos/Request/UpdateUserDto.cs
+++ LocaFilms/Dtos/Request/UpdateUserDto.cs
@@ -11,12 +11,6 @@ namespace LocaFilms.Dtos.Request
         [Required(ErrorMessage = "O campo 'LastName' é obrigatório.")]
         public string? LastName { get; set; }
 
-        [Required(ErrorMessage = "O campo 'UserName' é obrigatório.")]
-        public string? UserName { get; set; }
-
-        [Required(ErrorMessage = "O campo 'Email' é obrigatório.")]
-        public string? Email { get; set; }
-
         [Required(ErrorMessage = "O campo 'PhoneNumber' é obrigatório.")]
         public string? PhoneNumber { get; set; }
     }
